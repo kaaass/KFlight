24290dea46c9279a44bbf1565ba30864a8f42944
KAAAsS <admin@kaaass.net>
2020-03-31 15:31:56 +0800

    完成城市数据管理

--- src/main/java/net/kaaass/kflight/data/CityManager.java
+++ src/main/java/net/kaaass/kflight/data/CityManager.java
@@ -1,7 +1,49 @@
 package net.kaaass.kflight.data;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Synchronized;
+import net.kaaass.kflight.data.entry.EntryCity;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
 /**
  * 城市数据管理
  */
 public class CityManager {
+
+    private static final CityManager INSTANCE = new CityManager();
+
+    private List<EntryCity> data;
+
+    @Getter
+    private Index<EntryCity, String, Integer> indexName;
+
+    private CityManager() {
+        data = new ArrayList<>();
+        indexName = new Index<>(EntryCity::getName, String::hashCode, Comparator.naturalOrder());
+    }
+
+    /**
+     * 添加项目
+     */
+    @Synchronized
+    public static void addEntry(EntryCity entryCity) {
+        INSTANCE.data.add(entryCity);
+        INSTANCE.indexName.addIndexFor(entryCity);
+    }
+
+    /**
+     * 通过城市名查找
+     */
+    public static Optional<EntryCity> findByName(String name) {
+        return INSTANCE.indexName.findOne(name);
+    }
+
+    public static CityManager getInstance() {
+        return INSTANCE;
+    }
 }
--- src/main/java/net/kaaass/kflight/data/entry/EntryCity.java
+++ src/main/java/net/kaaass/kflight/data/entry/EntryCity.java
@@ -8,5 +8,8 @@ import lombok.Data;
 @Data
 public class EntryCity implements IEntry {
 
-    private String name;
+    /**
+     * 城市名
+     */
+    final String name;
 }
