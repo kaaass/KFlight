4fdad368127834059351d776131ceefe9a0d96ff
KAAAsS <admin@kaaass.net>
2020-04-10 20:46:11 +0800

    增加导入接口

--- src/main/java/net/kaaass/kflight/controller/FlightController.java
+++ src/main/java/net/kaaass/kflight/controller/FlightController.java
@@ -1,12 +1,17 @@
 package net.kaaass.kflight.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import net.kaaass.kflight.data.DataLoader;
 import net.kaaass.kflight.data.FlightManager;
 import net.kaaass.kflight.data.entry.EntryFlight;
+import net.kaaass.kflight.exception.BadRequestException;
 import net.kaaass.kflight.exception.NotFoundException;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/flight")
 public class FlightController {
@@ -47,6 +52,16 @@ public class FlightController {
             FlightManager.removeEntry(FlightManager.getById(id));
         } catch (IndexOutOfBoundsException e) {
             throw new NotFoundException("航班ID不存在");
+        }
+    }
+
+    @GetMapping("/import/")
+    void getFlightById(@RequestParam String filepath) throws BadRequestException {
+        try {
+            DataLoader.loadFlightFromJsonFile(filepath);
+        } catch (IOException e) {
+            log.warn("打开文件失败", e);
+            throw new BadRequestException("文件打开失败！请检查文件是否存在。");
         }
     }
 }
