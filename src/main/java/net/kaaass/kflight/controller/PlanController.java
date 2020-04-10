dcc3e5a3df09f27c006ac4ca29858646064d8e03
KAAAsS <admin@kaaass.net>
2020-04-10 21:04:30 +0800

    增加航班转机规划

---
+++ src/main/java/net/kaaass/kflight/controller/PlanController.java
@@ -0,0 +1,23 @@
+package net.kaaass.kflight.controller;
+
+import net.kaaass.kflight.data.CityManager;
+import net.kaaass.kflight.data.Planner;
+import net.kaaass.kflight.exception.NotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/plan")
+public class PlanController {
+
+    @GetMapping("/")
+    List<Planner.FlightPlan> plan(@RequestParam String from, @RequestParam String to, @RequestParam LocalDate date) throws NotFoundException {
+        var fromCity = CityManager.findByName(from)
+                .orElseThrow(() -> new NotFoundException("城市不存在！"));
+        var toCity = CityManager.findByName(to)
+                .orElseThrow(() -> new NotFoundException("城市不存在！"));
+        return Planner.plan(fromCity, toCity, date);
+    }
+}
