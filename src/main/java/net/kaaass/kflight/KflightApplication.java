f42e9c11d585c9246986bd384938afa790b4e641
KAAAsS <admin@kaaass.net>
2020-04-10 16:50:46 +0800

    添加Springboot依赖

---
+++ src/main/java/net/kaaass/kflight/KflightApplication.java
@@ -0,0 +1,13 @@
+package net.kaaass.kflight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KflightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KflightApplication.class, args);
+	}
+
+}
