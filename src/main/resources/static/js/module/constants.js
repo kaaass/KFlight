9d82b15aee513e1b4542741e054c10362dbb2660
KAAAsS <admin@kaaass.net>
2020-04-13 17:30:24 +0800

    增加网页基本结构

---
+++ src/main/resources/static/js/module/constants.js
@@ -0,0 +1,14 @@
+/**
+ * 常量声明
+ */
+define([], function () {
+
+    return {
+        TEMPLATE_PATH: '/app/templates/',
+        TEMPLATE_SUFFIX: '.hbs',
+
+        TITLE_SUFFIX: " - KFlight",
+
+        PARAM_ID: "id"
+    };
+});
